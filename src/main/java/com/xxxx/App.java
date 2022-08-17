edcfc6dea152b461b19eca412a072c785b500ee2
Lee <lee1998hy@163.com>
2022-08-17 20:33:24 +0800

    第一次

---
+++ src/main/java/com/xxxx/App.java
@@ -0,0 +1,9 @@
+package com.xxxx;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello test");
+        System.out.println("hello git");
+    }
+}
